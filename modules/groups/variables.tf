5e1964684d81d8a9a2643d7cdbecc7aa43aad755
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-20 16:47:02 +0530

    Create variables.tf

---
+++ modules/groups/variables.tf
@@ -0,0 +1,9 @@
+variable "group_name" {
+  description = "Name of the group"
+  type        = string
+}
+
+variable "user_object_id" {
+  description = "Object ID of the user to add to group"
+  type        = string
+}
